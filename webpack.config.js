c2afa27aa52b149aea60da296ee02a1a5d68dd08
Stephen McCann <info@flatsteve.com>
2018-09-10 21:37:05 +0100

    Add service worker and icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const PUBLIC_PATH = "https://flatsteve.com/brolly/";
 
 module.exports = {
   devtool: "source-map",
@@ -33,6 +37,15 @@ module.exports = {
       template: "./src/index.html",
       filename: "./index.html",
       favicon: "./public/favicon.ico"
-    })
+    }),
+    new SWPrecacheWebpackPlugin({
+      cacheId: "my-project-name",
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: "service-worker.js",
+      minify: true,
+      navigateFallback: PUBLIC_PATH + "index.html",
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+    }),
+    new CopyWebpackPlugin([{ from: "public/icons", to: "icons" }])
   ]
 };
