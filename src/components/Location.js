9a71b797c348270793cd1aab9fb706bf0f97c4b5
Stephen McCann <info@flatsteve.com>
2018-09-28 22:49:39 +0100

    Location select (#3)

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,15 +1,71 @@
 import React, { Component } from "react";
+import debounce from "lodash-es/debounce";
 
+import { searchLocations } from "../services/api";
 import { getClosestLocation } from "../services/geolocation";
 import { withStore } from "../data/store";
 import locationIcon from "../icons/location.svg";
 import Spinner from "./common/Spinner";
+import closeIcon from "../icons/close.svg";
 
 import "./Location.scss";
 
 export class Location extends Component {
+  constructor(props) {
+    super(props);
+
+    this.queryLocations = debounce(this.queryLocations, 500);
+  }
+
   state = {
-    loading: false
+    loading: false,
+    expanded: false,
+    search: this.props.store.location.name || "",
+    results: []
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.store.location.id !== this.props.store.location.id) {
+      this.setState({
+        search: this.props.store.location.name
+      });
+    }
+  }
+
+  queryLocations = search => {
+    this.setState({
+      loading: true
+    });
+
+    searchLocations(search)
+      .then(res => {
+        this.setState({
+          results: res.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        this.setState({
+          loading: false
+        });
+      });
+  };
+
+  handleSearch = e => {
+    const search = e.target.value;
+
+    this.setState({
+      search
+    });
+
+    this.queryLocations(search);
+  };
+
+  setLocation = location => {
+    this.props.store.updateLocation(location);
+    this.setExpanded(false);
   };
 
   requestLocation = () => {
@@ -31,23 +87,85 @@ export class Location extends Component {
       });
   };
 
+  setExpanded = expanded => {
+    this.setState({
+      expanded
+    });
+  };
+
+  close = () => {
+    this.setState({
+      expanded: false,
+      search: this.props.store.location.name
+    });
+  };
+
   render() {
-    const { loading } = this.state;
-    const { location } = this.props.store;
+    const { loading, expanded, results, search } = this.state;
 
     return (
-      <div className="location">
-        <input className="location-input" value={location.name} />
+      <div
+        className={`location ${
+          expanded ? "location--expanded" : "locaton--colapsed"
+        }`}
+      >
+        <div className="location__container">
+          {expanded && (
+            <div className="location__header">
+              <h4 className="location__title">Set your location</h4>
 
-        {loading ? (
-          <Spinner />
-        ) : (
-          <div
-            className="location-icon"
-            onClick={() => this.requestLocation()}
-            dangerouslySetInnerHTML={{ __html: locationIcon }}
-          />
-        )}
+              <div
+                onClick={this.close}
+                className="location__close"
+                dangerouslySetInnerHTML={{ __html: closeIcon }}
+              />
+            </div>
+          )}
+
+          <div className="location__control">
+            <input
+              className="location__control__input"
+              value={search}
+              onChange={this.handleSearch.bind(this)}
+              onClick={() => this.setExpanded(true)}
+            />
+
+            <div className="location__control__extra">
+              {loading ? (
+                <Spinner />
+              ) : (
+                <div
+                  className="location-icon"
+                  onClick={() => this.requestLocation()}
+                  dangerouslySetInnerHTML={{ __html: locationIcon }}
+                />
+              )}
+            </div>
+          </div>
+
+          {expanded && (
+            <div>
+              {results.length ? (
+                <div className="location-results">
+                  {results.map(location => {
+                    return (
+                      <p
+                        key={location.id}
+                        onClick={() => this.setLocation(location)}
+                      >
+                        {location.name}
+                      </p>
+                    );
+                  })}
+                </div>
+              ) : (
+                <p className="location-results-empty">
+                  No results found, try searching for a differnet location...
+                </p>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     );
   }
