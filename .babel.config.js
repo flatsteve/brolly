977222335128fae8987c23b10d4510a6838faa69
Stephen McCann <info@flatsteve.com>
2019-02-11 21:56:54 +0000

    Rename babel config

--- .babel.config.js
+++ .babel.config.js
@@ -1,7 +1,7 @@
-{
-  "presets": ["@babel/preset-env", "@babel/preset-react"],
-  "plugins": [
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-object-rest-spread"
   ]
-}
+};
