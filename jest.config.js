3e6c64def0eb5a1046b0ee775a10634e67411889
Stephen McCann <info@flatsteve.com>
2019-02-11 20:59:01 +0000

    Remove enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  setupFiles: ["./tests/setupTests.js"],
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "<rootDir>/tests/mocks/styleMock.js",
     "\\.(gif|ttf|eot|svg)$": "<rootDir>/tests/mocks/fileMock.js"
